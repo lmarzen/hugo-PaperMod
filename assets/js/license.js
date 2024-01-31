6de3bccb7ecea05ab59d3044fb4455fdda38163b
Luke Marzen <98473324+lmarzen@users.noreply.github.com>
2024-01-30 23:30:31 -0600

    added MIT license for my modifications

--- assets/js/license.js
+++ assets/js/license.js
@@ -1,6 +1,7 @@
 /*
-  PaperMod v7
-  License: MIT https://github.com/adityatelange/hugo-PaperMod/blob/master/LICENSE
+  PaperMod v7 (modified)
+  License: MIT https://github.com/lmarzen/hugo-PaperMod/blob/master/LICENSE
   Copyright (c) 2020 nanxiaobei and adityatelange
   Copyright (c) 2021-2024 adityatelange
+  Copyright (c) 2023-2024 Luke Marzen
 */
